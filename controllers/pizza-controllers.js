500213f17d8d1b20c004715dec27140600e1a577
lorreina <slorreina369@gmail.com>
2022-08-30 15:55:54 -0700

    can leave comments on pizzas

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -3,14 +3,25 @@ const { Pizza } = require('../models');
 const pizzaController = {
     getAllPizza(req,res){
         Pizza.find({})
-            .then(dbPizzaData => res.json(dbPizzaData))
-            .catch(err =>{
-                console.log(err);
-                res.status(400).json(err);
-            });
+        .populate({
+            path:'comments',
+            select:'-_v'
+        })
+        .select('-_v')
+        .sort({_id:-1})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err =>{
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     getPizzaById({params}, res){
         Pizza.findOne({_id:params.id})
+        .populate({
+            path:'comments',
+            select:'-_v'
+        })
+        .select('-_v')
         .then((dbPizzaData =>{
             if(!dbPizzaData){
                 res.status(404).json({message:'No pizza found with this id'});
