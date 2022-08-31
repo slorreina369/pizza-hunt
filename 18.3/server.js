253208411dae395e3ff340e92c87eaf8fbb891dd
lorreina <slorreina369@gmail.com>
2022-08-31 11:24:22 -0700

    reply function created

--- 18.3/server.js
+++ 18.3/server.js
@@ -7,14 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
-}); 
-=======
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt',
   {
@@ -22,9 +15,10 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
->>>>>>> 6c6975a0ef729ac6fd0b4a4cd6dbd13360373a4d
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
